0da26bc9582dcaa2aafd112be39ea9ece335985a
Fauzan Akmal Mahdi <recovery252@gmail.com>
2025-02-01 14:37:02 +0700

    update project page

--- src/routes/dashboard/ProjectPage.jsx
+++ src/routes/dashboard/ProjectPage.jsx
@@ -1,9 +1,161 @@
+import { motion } from "framer-motion";
 import React from "react";
+import aboutImg from "@/assets/profile-image2.jpg";
+import PricingItemComponent from "./component/PricingItemComponent";
+import { HiBadgeCheck } from "react-icons/hi";
+
+const listItemPricingData = [
+    {
+        name: "Silver",
+        icon: <HiBadgeCheck className="size-9 fill-white" />,
+        price: "Rp1.000.000",
+        listItems: [
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+        ],
+    },
+    {
+        name: "Gold",
+        icon: <HiBadgeCheck className="size-9 fill-white" />,
+        price: "Rp2.000.000",
+        listItems: [
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+        ],
+    },
+    {
+        name: "Diamond",
+        icon: <HiBadgeCheck className="size-9 fill-white" />,
+        price: "Rp3.000.000",
+        listItems: [
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+        ],
+    },
+    {
+        name: "Silver",
+        icon: <HiBadgeCheck className="size-9 fill-white" />,
+        price: "Rp1.000.000",
+        listItems: [
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+        ],
+    },
+    {
+        name: "Gold",
+        icon: <HiBadgeCheck className="size-9 fill-white" />,
+        price: "Rp2.000.000",
+        listItems: [
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+        ],
+    },
+    {
+        name: "Diamond",
+        icon: <HiBadgeCheck className="size-9 fill-white" />,
+        price: "Rp3.000.000",
+        listItems: [
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+        ],
+    },
+    {
+        name: "Silver",
+        icon: <HiBadgeCheck className="size-9 fill-white" />,
+        price: "Rp1.000.000",
+        listItems: [
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+        ],
+    },
+    {
+        name: "Gold",
+        icon: <HiBadgeCheck className="size-9 fill-white" />,
+        price: "Rp2.000.000",
+        listItems: [
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+        ],
+    },
+    {
+        name: "Diamond",
+        icon: <HiBadgeCheck className="size-9 fill-white" />,
+        price: "Rp3.000.000",
+        listItems: [
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+        ],
+    },
+    {
+        name: "Silver",
+        icon: <HiBadgeCheck className="size-9 fill-white" />,
+        price: "Rp1.000.000",
+        listItems: [
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+        ],
+    },
+    {
+        name: "Gold",
+        icon: <HiBadgeCheck className="size-9 fill-white" />,
+        price: "Rp2.000.000",
+        listItems: [
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+        ],
+    },
+    {
+        name: "Diamond",
+        icon: <HiBadgeCheck className="size-9 fill-white" />,
+        price: "Rp3.000.000",
+        listItems: [
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+        ],
+    },
+];
 
 const ProjectPage = () => {
     return (
-        <div>
-            <h1 className="title">ProjectPage</h1>
+        <div className="border-b border-neutral-900 pb-4">
+            <motion.h2
+                whileInView={{ opacity: 1, y: 0 }}
+                initial={{ opacity: 0, y: -100 }}
+                transition={{ duration: 1.5 }}
+                className="title my-20 text-center text-4xl"
+            >
+                Project Page
+            </motion.h2>
+            <div className="flex flex-wrap">
+                <div className="mt-8 grid w-full grid-cols-1 items-start justify-center gap-8 sm:grid-cols-2 lg:grid-cols-4">
+                    {listItemPricingData.map((element) => (
+                        <motion.div
+                            whileInView={{ opacity: 1, y: 0 }}
+                            initial={{ opacity: 0, y: 100 }}
+                            transition={{ duration: 1.5 }}
+                        >
+                            <PricingItemComponent
+                                name={element.name}
+                                icon={element.icon}
+                                price={element.price}
+                                listItems={element.listItems}
+                            />
+                        </motion.div>
+                    ))}
+                </div>
+            </div>
         </div>
     );
 };
