ecb744622f49d5939e3a96ffd93189c9e3b6109d
Fauzan Akmal Mahdi <recovery252@gmail.com>
2025-02-01 19:50:27 +0700

    update experience page

---
+++ src/constants/experience.jsx
@@ -0,0 +1,75 @@
+export const HERO_CONTENT = `Software engineer over three years experience in full-stack mobile development (flutter & golang). Challenged to take complexity and turn it into simple things. Able to break down concepts and communicatively to deliver objectivity into team progress. Handled threading process on image processing and face recognition in flutter, developed & deployed backend service, and I have worked with the implementation of machine learning models into mobile applications.`;
+
+export const ABOUT_TEXT = `With 3 years of hands-on experience as a software engineer, I thrive on the challenge of turning complex problems into elegant solutions. My expertise spans a dynamic tech stack, including Flutter for creating captivating frontend mobile applications, Golang for building robust backend systems, and Python for pioneering developments in machine learning. Additionally, I have a solid foundation in OOP programming languages like Java and Kotlin, enhancing my versatility as an engineer.
+
+In 2022, I proudly graduated with a bachelor's degree in Computer Science, specializing in the fascinating realm of Deep Learning. Throughout my academic journey, I maintained an outstanding GPA of 3.86, a testament to my commitment to excellence. My passion for innovation led me to embark on a remarkable thesis, where I implemented Convolutional Neural Networks (CNN) to identify Taekwondo Martial Arts Movement. This groundbreaking research has been recognized and accepted as a research paper by the prestigious Institute of Electrical and Electronics Engineers (IEEE).
+
+In Taekwondo Martial Arts: Beyond the realm of technology, I immerse myself in the discipline of Taekwondo. As a dedicated practitioner with a black belt, I embrace the ethos of perseverance, respect, and continuous growth. My thesis project exemplifies my dedication to combining my passion for martial arts with my IT skill set to tackle real-world challenges.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2023 - Present",
+    role: "Senior Full Stack Developer",
+    company: "Google Inc.",
+    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
+    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+  },
+  {
+    year: "2022 - 2023",
+    role: "Frontend Developer",
+    company: "Adobe",
+    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
+    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+  },
+  {
+    year: "2021 - 2022",
+    role: "Full Stack Developer",
+    company: "Facebook",
+    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
+    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+  },
+  {
+    year: "2020 - 2021",
+    role: "Software Engineer",
+    company: "Paypal",
+    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
+    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+  },
+];
+
+// export const PROJECTS = [
+//   {
+//     title: "E-Commerce Website",
+//     image: project1,
+//     description:
+//       "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+//     technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+//   },
+//   {
+//     title: "Task Management App",
+//     image: project2,
+//     description:
+//       "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+//     technologies: ["HTML", "CSS", "Angular", "Firebase"],
+//   },
+//   {
+//     title: "Portfolio Website",
+//     image: project3,
+//     description:
+//       "A personal portfolio website showcasing projects, skills, and contact information.",
+//     technologies: ["HTML", "CSS", "React", "Bootstrap"],
+//   },
+//   {
+//     title: "Blogging Platform",
+//     image: project4,
+//     description:
+//       "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+//     technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+//   },
+// ];
+
+export const CONTACT = {
+  address: "767 Fifth Avenue, New York, NY 10153 ",
+  phoneNo: "+12 4555 666 00 ",
+  email: "me@example.com",
+};
--- src/routes/dashboard/ExperiencePage.jsx
+++ src/routes/dashboard/ExperiencePage.jsx
@@ -1,9 +1,65 @@
 import React from "react";
+import { motion } from "framer-motion";
+import { EXPERIENCES } from "../../constants/experience";
 
 const ExperiencePage = () => {
     return (
-        <div>
-            <h1 className="title">ExperiencePage</h1>
+        <div className="border-b border-neutral-900 pb-4">
+            {/* Title Section */}
+            <motion.h2
+                whileInView={{ opacity: 1, y: 0 }}
+                initial={{ opacity: 0, y: -100 }}
+                transition={{ duration: 1.5 }}
+                className="title my-20 text-center text-4xl"
+            >
+                Experience
+            </motion.h2>
+
+            {/* Detail Section */}
+            <div>
+                {EXPERIENCES.map((item, index) => (
+                    <div
+                        key={index}
+                        className="mb-8 flex flex-wrap lg:justify-center"
+                    >
+                        {/* Year Section */}
+                        <motion.div
+                            whileInView={{ opacity: 1, x: 0 }}
+                            initial={{ opacity: 0, x: -100 }}
+                            transition={{ duration: 1 }}
+                            className="w-full lg:w-1/4"
+                        >
+                            <p className="title mb-2 text-sm">{item.year}</p>
+                        </motion.div>
+
+                        {/* Experience Desc Section */}
+                        <motion.div
+                            whileInView={{ opacity: 1, x: 0 }}
+                            initial={{ opacity: 0, x: 100 }}
+                            transition={{ duration: 1 }}
+                            className="w-full max-w-xl lg:w-3/4"
+                        >
+                            <h6 className="title mb-2 text-base font-semibold">
+                                {item.role} - <span className="title text-sm">{item.company}</span>
+                            </h6>
+                            <p className="description mb-4">{item.description}</p>
+                            <div className="space-x-2">
+                                {item.technologies.map((itemTech, index) => (
+                                    <div className="card inline-flex p-2">
+                                        <h2 className="description">{itemTech}</h2>
+                                    </div>
+                                    // <span
+                                    //     key={index}
+                                    //     className="mr-2 mt-4 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800"
+                                    // >
+                                    //     {itemTech}
+                                    // </span>
+                                ))}
+                            </div>
+                        </motion.div>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
