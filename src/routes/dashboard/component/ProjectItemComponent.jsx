f85926fc01f24982d07ec79ca6f3e3f1046e0632
Fauzan Akmal Mahdi <recovery252@gmail.com>
2025-02-06 00:45:58 +0700

    improve project item component see detail

--- src/routes/dashboard/component/ProjectItemComponent.jsx
+++ src/routes/dashboard/component/ProjectItemComponent.jsx
@@ -30,59 +30,25 @@ const ProjectItemComponent = ({ name, image, desc }) => {
         <div
             data-aos="zoom-in"
             data-aos-delay="600"
-            className="flex h-[550px] w-full cursor-pointer flex-col items-center justify-start gap-6 overflow-hidden rounded-lg border-2 border-gray-700 hover:border-slate-500 hover:bg-slate-200 dark:hover:border-slate-500 dark:hover:bg-slate-900"
+            className="relative flex h-[550px] w-full cursor-pointer flex-col items-center justify-start gap-6 overflow-hidden rounded-lg border-2 border-gray-700 hover:border-slate-500 hover:bg-slate-200 dark:hover:border-slate-500 dark:hover:bg-slate-900"
         >
-            <div className="max-h-44 overflow-hidden">
-                <img
-                    src={image}
-                    alt=""
-                    className="clip-half object-cover"
-                />
-            </div>
-            <div className="flex w-full flex-col items-center justify-center gap-6 rounded-lg px-4 py-6">
-                {/* <div className="max-h-40 w-64 overflow-hidden rounded-lg bg-blue-200 p-4 shadow-md">
-                    <p>
-                        This is a flexible height div. If the content exceeds the max height, it will be hidden. Add more text to see the effect. The
-                        text should be truncated after a certain height. This is a flexible height div. If the content exceeds the max height, it will
-                        be hidden. Add more text to see the effect. The text should be truncated after a certain height. This is a flexible height
-                        div. If the content exceeds the max height, it will be hidden. Add more text to see the effect. The text should be truncated
-                        after a certain height.
-                    </p>
-                </div> */}
-                {/* <h1 className="font-poppins text-center text-[24px] font-semibold text-white">{name}</h1> */}
-                {/* <div className="max-h-full min-h-20 w-64 bg-gray-300 p-4">
-                    <div className="max-h-full min-h-20 overflow-hidden rounded-lg bg-blue-200 p-4 shadow-md">
-                        <p>
-                            This div will have a minimum height but won’t exceed the parent’s height. If the text is too long, it will be hidden
-                            instead of growing indefinitely. This div will have a minimum height but won’t exceed the parent’s height. If the text is
-                            too long, it will be hidden instead of growing indefinitely. This div will have a minimum height but won’t exceed the
-                            parent’s height. If the text is too long, it will be hidden instead of growing indefinitely.
-                        </p>
-                    </div>
-                </div> */}
-
-                <h1 className="title text-center">{name}</h1>
-                <div className="flex max-h-36 w-full flex-col items-center justify-center gap-2 overflow-hidden text-white">
-                    <p className="description line-clamp-5">{convertLinks(desc)}</p>
+            <div className="flex flex-col">
+                <div className="max-h-44 overflow-hidden">
+                    <img
+                        src={image}
+                        alt=""
+                        className="clip-half object-cover"
+                    />
                 </div>
-                <div className="title text-base">See Detail</div>
-                {/* <div className="flex h-64 w-64 items-end justify-end bg-gray-800 p-4">
-                    <div className="title text-base">Child Element</div>
-                </div> */}
-                {/* <div className="bottom-0 right-0 rounded-lg bg-blue-500 p-2 text-white">Child Element</div> */}
-
-                {/* <div className="max-h-full min-h-20 overflow-hidden rounded-lg bg-blue-200 p-4 shadow-md">
-                    <p>
-                        This div will have a minimum height but won’t exceed the parent’s height. If the text is too long, it will be hidden instead
-                        of growing indefinitely.
-                    </p>
-                </div> */}
-                {/* <button className="hover:bg-themered hover:border-themered font-poppins mt-5 flex items-center justify-center gap-2 rounded-lg border-2 border-slate-300 px-8 py-2 text-[15px] font-semibold text-white dark:border-slate-700">
-                    <p className="description">See More</p>
-                    <span>
-                        <FaArrowRightLong className="description" />
-                    </span>{" "}
-                </button> */}
+                <div className="flex w-full flex-col items-center justify-center gap-6 rounded-lg px-4 py-6">
+                    <h1 className="title text-center">{name}</h1>
+                    <div className="flex max-h-36 w-full flex-col items-center justify-center gap-2 overflow-hidden text-white">
+                        <p className="description line-clamp-5">{convertLinks(desc)}</p>
+                    </div>
+                </div>
+            </div>
+            <div className="md:hidden absolute bottom-5 right-5">
+                <h1 className="title text-base">See Detail</h1>
             </div>
         </div>
     );
